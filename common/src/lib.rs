ed5d88c5cfd3218fa20e8fa1c24a82645229189d
Alejandro Alvarado <alejalvarado@paypal.com>
2021-10-14 22:59:07 -0600

    [master] start core game logic implementation

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,8 +3,8 @@ pub mod board {
     use std::cmp::{min, max};
 
     pub type TileEndpoint = usize;
-
     /// # Tiles
+    pub const BOARD_LENGTH:usize = 6;
     ///  A tile has 4 pairs of endpoints, with each pair connected by a `Segment`.
     ///  We represent a `Tile` as a collection of four `Segment`s, which are just pairs of endpoints.
     ///  Endpoints are identified by a number representing their position on a tile.
@@ -65,11 +65,119 @@ pub mod board {
         }
     }
 
+    /// Position (row, col) of a cell in the board, where a tile may be placed
+    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+    pub struct CellPos {
+        pub row: usize,
+        pub col: usize
+    }
+
+    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+    pub struct PlayerPos {
+        pub cell: CellPos,
+        pub endpoint: TileEndpoint
+    }
+
+    pub struct Player {
+        pub pos: PlayerPos,
+        // name, id or sumthing
+        // hand
+        // dragon: bool?
+    }
+
+    pub struct Move {
+        pub tile: Tile,
+        pub cell: CellPos
+    }
+
+    pub struct BoardState {
+        //setup: Vec<SetupTurn|Player>  or something reflecting initial conditions?
+        history: Vec<Move>,
+        alive_players: Vec<Player>,
+        //deck
+    }
+
+    impl BoardState {
+        pub fn tile_at(&self, row:usize, col:usize) -> Option<Tile>{
+            self.history
+                .iter()
+                .find(|&mov| mov.cell.row == row && mov.cell.col == col)
+                .map(|mov| mov.tile)
+        }
+    }
+
+    // server code
+
+    impl BoardState {
+        fn process_move(&mut self, mov:Move) {
+            self.history.push(mov);
+
+            for player in &self.alive_players {
+
+            }
+        }
+
+        fn facing_pos(&self, current:&PlayerPos) -> PlayerPos {
+            let (current_row, current_col) = (current.cell.row, current.cell.col);
+            // if there is no tile at the player's position, they are at their starting position
+            if self.tile_at(current_row, current_col).is_none() {
+                return current.clone();
+            }
+            let (min, max) = (0usize, BOARD_LENGTH - 1);
+            // otherwise find the row,col and entry in front of them
+            let (row, col) = match (current.endpoint, current_row, current_col) {
+                // They are on the edge of their starting position
+                (0 | 1, row, col) if row == max => (row, col),
+                (2 | 3, row, col) if col == max => (row, col),
+                (4 | 5, row, col) if row == min => (row, col),
+                (6 | 7, row, col) if col == min => (row, col),
+                // They are inside the board
+                (0 | 1, row, col) => (row + 1, col),
+                (2 | 3, row, col) => (row, col + 1),
+                (4 | 5, row, col) => (row - 1, col),
+                (6 | 7, row, col) => (row, col - 1),
+                _ => panic!("non existent entry {}",  current.endpoint)
+            };
+
+            let facing_entry:TileEndpoint = if (row, col) == (current_row, current_col) {
+                // this was their first move, so they are facing at the tile they just placed and not moved yet
+                current.endpoint
+            } else {
+                //get the endpoint of the facing cell connected to the current endpoint
+                match current.endpoint {
+                    0 => 5,
+                    1 => 4,
+                    2 => 7,
+                    3 => 6,
+                    4 => 1,
+                    5 => 0,
+                    6 => 3,
+                    7 => 2,
+                    _ => panic!("non existent entry {}",  current.endpoint)
+                }
+            };
+            PlayerPos { cell: CellPos {row, col},  endpoint: facing_entry}
+        }
+    }
+
+
+    #[test]
+    fn facing_empty_cell_board_edge() {
+        let board = BoardState {history: Vec::new(), alive_players: Vec::new()};
+
+        let start_pos = PlayerPos { cell: CellPos { row: 0, col: 0 }, endpoint: 0 };
+        assert_eq!(board.facing_pos(&start_pos), start_pos);
+
+        let start_pos = PlayerPos { cell: CellPos { row: 1, col: 0 }, endpoint: 0 };
+        assert_eq!(board.facing_pos(&start_pos), start_pos);
+        // Finish me...
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::board::*;
+    use std::io::empty;
 
     fn seg(a: TileEndpoint, b:TileEndpoint) -> Segment {
         Segment::new(a,b)
@@ -102,5 +210,6 @@ mod tests {
         assert_eq!(tile.rotated(false), Tile::new([seg(1,7), seg(2,3), seg(0,4), seg(5,6)]));
         assert_eq!(tile.rotated(false).rotated(false).rotated(false).rotated(false),tile);
     }
+
 
 }
